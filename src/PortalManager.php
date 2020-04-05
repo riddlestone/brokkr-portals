79dee88daed198dafb11be2a6c2930206897764d
Andrew Riddlestone <andrew.riddlestone@gmail.com>
2020-04-05 11:16:04 +0100

    feature(portal-manager): Added hasPortalConfig() and hasCurrentPortalConfig()

--- src/PortalManager.php
+++ src/PortalManager.php
@@ -55,6 +55,23 @@ class PortalManager
     }
 
     /**
+     * Get whether configuration exists for the named portal
+     *
+     * @param string $name
+     * @param string|null $configKey
+     * @return bool
+     */
+    public function hasPortalConfig(string $name, ?string $configKey = null): bool
+    {
+        foreach($this->portalConfigProviders as $provider) {
+            if ($provider->hasConfiguration($name, $configKey)) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    /**
      * Get the configuration for the named portal
      *
      * @param string $name
@@ -80,6 +97,17 @@ class PortalManager
     public function getCurrentPortalName(): string
     {
         return $this->currentPortalName;
+    }
+
+    /**
+     * Get whether configuration exists for the currently selected portal
+     *
+     * @param string|null $configKey
+     * @return bool
+     */
+    public function hasCurrentPortalConfig(?string $configKey = null): bool
+    {
+        return $this->hasPortalConfig($this->currentPortalName, $configKey);
     }
 
     /**
