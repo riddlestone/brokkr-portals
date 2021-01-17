5b3db02232d85da33b507195715ada64b36fda29
Andrew Riddlestone <andrew.riddlestone@gmail.com>
2021-01-17 13:37:58 +0000

    feat(features): Allow features to depend on other features
    Closes #6

--- test/IntegrationTest.php
+++ test/IntegrationTest.php
@@ -2,6 +2,7 @@
 
 namespace Riddlestone\Brokkr\Portals\Test;
 
+use Exception;
 use Laminas\Config\Config;
 use Laminas\ServiceManager\ServiceManager;
 use PHPUnit\Framework\TestCase;
@@ -56,6 +57,93 @@ class IntegrationTest extends TestCase
                 'package.css',
             ],
             $portalManager->getPortalConfig('admin', 'css')
+        );
+    }
+
+    public function featureTreeData(): array
+    {
+        return [
+            'simple dependency' => [
+                [
+                    'portals' => ['main' => ['features' => ['feature1']]],
+                    'portal_features' => [
+                        'feature1' => ['features' => ['feature2'], 'css' => ['feature1.css']],
+                        'feature2' => ['css' => ['feature2.css']],
+                    ],
+                ],
+                [
+                    'feature2.css',
+                    'feature1.css',
+                ],
+            ],
+            'common dependency' => [
+                [
+                    'portals' => ['main' => ['features' => ['feature1', 'feature2']]],
+                    'portal_features' => [
+                        'feature1' => ['features' => ['feature3'], 'css' => ['feature1.css']],
+                        'feature2' => ['features' => ['feature3'], 'css' => ['feature2.css']],
+                        'feature3' => ['css' => ['feature3.css']],
+                    ],
+                ],
+                [
+                    'feature3.css',
+                    'feature1.css',
+                    'feature2.css',
+                ],
+            ],
+            'resource type only in dependency' => [
+                [
+                    'portals' => ['main' => ['features' => ['feature1', 'feature2']]],
+                    'portal_features' => [
+                        'feature1' => ['features' => ['feature3']],
+                        'feature2' => ['features' => ['feature3']],
+                        'feature3' => ['css' => ['feature3.css']],
+                    ],
+                ],
+                [
+                    'feature3.css',
+                ],
+            ],
+            'chained dependencies' => [
+                [
+                    'portals' => ['main' => ['features' => ['feature1']]],
+                    'portal_features' => [
+                        'feature1' => ['features' => ['feature2'], 'css' => ['feature1.css']],
+                        'feature2' => ['features' => ['feature3'], 'css' => ['feature2.css']],
+                        'feature3' => ['css' => ['feature3.css']],
+                    ],
+                ],
+                [
+                    'feature3.css',
+                    'feature2.css',
+                    'feature1.css',
+                ],
+            ],
+        ];
+    }
+
+    /**
+     * Test features which depend on other features
+     *
+     * @dataProvider featureTreeData
+     * @param array $configData
+     * @param array $expectedResult
+     * @throws Exception
+     */
+    public function testFeatureTreeExample(array $configData, array $expectedResult)
+    {
+        $module = new Module();
+        $config = new Config($module->getConfig());
+        $config->merge(
+            new Config($configData)
+        );
+        $serviceManager = new ServiceManager($config->toArray()['service_manager']);
+        $serviceManager->setService('Config', $config->toArray());
+        /** @var PortalManager $portalManager */
+        $portalManager = $serviceManager->get(PortalManager::class);
+        $this->assertEquals(
+            $expectedResult,
+            $portalManager->getPortalConfig('main', 'css')
         );
     }
 }
